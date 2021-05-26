0a801431de2b00bfca99f4dfd2216a753c91e1b8
yil8853 <50975949+yil8853@users.noreply.github.com>
2021-05-26 13:15:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I love you ISU!');
+    res.send('I love you JSH!');
 });
 
 app.get('/start', function(req, res){
